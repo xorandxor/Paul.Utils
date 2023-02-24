46fedaac90f5777d5f386fbb465dc4ccda60a78f
xorandxor <paulreade738@gmail.com>
2023-02-24 10:47:14 -0800

    comments

--- AccountBalance.cs
+++ AccountBalance.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Security.Policy;
 using System.Data;
 using System.Data.SqlClient;
-using Newtonsoft.Json.Linq;
 using System.Globalization;
 
 namespace Paul.Utils
 {
+   
+    /// <summary>
+    /// This class encapsulates the various data assocuated with the accounts you have on kraken
+    /// </summary>
     public class BalanceObject
     {
         public BalanceObject()
@@ -23,13 +25,25 @@ namespace Paul.Utils
         public double ZEC { get; set; }
         public double REP { get; set; }
     }
+
     public class AccountBalance
     {
+
+        /// <summary>
+        /// Deserialize json data into a class object that can be access and passed around
+        /// </summary>
+        /// <param name="myJsonResponse">Ugly json data</param>
+        /// <returns>pretty c# class</returns>
         public static Root GetClassFromJson(string myJsonResponse)
         {
             return Newtonsoft.Json.JsonConvert.DeserializeObject<Root>(myJsonResponse);
         }
 
+        /// <summary>
+        /// This method displays the current number of coins in all the wallets as well as the value of each 
+        /// coin and also the total value of the portfolio... additionally, this method saves the account 
+        /// balance to the database
+        /// </summary>
         public static void GetAccountBalance()
         {
             string privateEndpoint = "Balance";
@@ -39,7 +53,6 @@ namespace Paul.Utils
 
             string privateResponse = API.QueryPrivateEndpoint(privateEndpoint, privateInputParameters, apiPublicKey, apiPrivateKey);
             Root acctbal = GetClassFromJson(privateResponse);
-
 
             BalanceObject bal = new BalanceObject();
             if (acctbal.result.ZUSD != null)
@@ -74,9 +87,9 @@ namespace Paul.Utils
             {
                 bal.ZEC = Convert.ToDouble(acctbal.result.XZEC.ToString());
             }
-            if (acctbal.result.XREP != null) 
-            { 
-                bal.REP = Convert.ToDouble(acctbal.result.XREP.ToString()); 
+            if (acctbal.result.XREP != null)
+            {
+                bal.REP = Convert.ToDouble(acctbal.result.XREP.ToString());
             }
 
             Logging.LogDB("Account Balance");
@@ -89,8 +102,8 @@ namespace Paul.Utils
             portfolio_value += GetAssetValueUSD("XMRUSD") * bal.XMR;
             portfolio_value += GetAssetValueUSD("DASHUSD") * bal.DASH;
             portfolio_value += GetAssetValueUSD("XZECUSD") * bal.ZEC;
-            portfolio_value += GetAssetValueUSD("XREPZUSD") * bal.REP ;
-            
+            portfolio_value += GetAssetValueUSD("XREPZUSD") * bal.REP;
+
             //whoops dont forget dollars
             portfolio_value += bal.USD;
 
@@ -98,40 +111,54 @@ namespace Paul.Utils
             Console.WriteLine("* ACCOUNT BALANCE                      ** ");
             Console.WriteLine("****************************************** ");
             Console.WriteLine("*      usd: " + bal.USD);
-            Console.WriteLine("*  bitcoin: " + bal.BTC  + " [$" + (GetAssetValueUSD("XXBTZUSD") * bal.BTC) +"]");
-            Console.WriteLine("* litecoin: " + bal.LTC  + " [$" + GetAssetValueUSD("XLTCUSD") * bal.LTC + "]");
-            Console.WriteLine("* ethereum: " + bal.ETH  + " [$" + GetAssetValueUSD("XETHZUSD") * bal.ETH+ "]");
-            Console.WriteLine("* dogecoin: " + bal.DGE  + " [$" + GetAssetValueUSD("XDGUSD")*bal.DGE + "]");
-            Console.WriteLine("*   monero: " + bal.XMR  + " [$" + GetAssetValueUSD("XMRUSD") * bal.XMR+ "]");
-            Console.WriteLine("*     dash: " + bal.DASH + " [$" + GetAssetValueUSD("DASHUSD") * bal.DASH + "]"); 
-            Console.WriteLine("*   z-cash: " + bal.ZEC  + " [$" + GetAssetValueUSD("XZECUSD") * bal.ZEC+ "]");
-            Console.WriteLine("*    augur: " + bal.REP  + " [$" + GetAssetValueUSD("XREPZUSD") * bal.REP + "]");
+            Console.WriteLine("*  bitcoin: " + bal.BTC + " [$" + (GetAssetValueUSD("XXBTZUSD") * bal.BTC) + "]");
+            Console.WriteLine("* litecoin: " + bal.LTC + " [$" + GetAssetValueUSD("XLTCUSD") * bal.LTC + "]");
+            Console.WriteLine("* ethereum: " + bal.ETH + " [$" + GetAssetValueUSD("XETHZUSD") * bal.ETH + "]");
+            Console.WriteLine("* dogecoin: " + bal.DGE + " [$" + GetAssetValueUSD("XDGUSD") * bal.DGE + "]");
+            Console.WriteLine("*   monero: " + bal.XMR + " [$" + GetAssetValueUSD("XMRUSD") * bal.XMR + "]");
+            Console.WriteLine("*     dash: " + bal.DASH + " [$" + GetAssetValueUSD("DASHUSD") * bal.DASH + "]");
+            Console.WriteLine("*   z-cash: " + bal.ZEC + " [$" + GetAssetValueUSD("XZECUSD") * bal.ZEC + "]");
+            Console.WriteLine("*    augur: " + bal.REP + " [$" + GetAssetValueUSD("XREPZUSD") * bal.REP + "]");
             Console.WriteLine("****************************************** ");
-            Console.WriteLine("* Total Portfolio Value: " + portfolio_value.ToString("C",CultureInfo.CurrentCulture));
+            Console.WriteLine("* Total Portfolio Value: " + portfolio_value.ToString("C", CultureInfo.CurrentCulture));
             Console.WriteLine("****************************************** ");
 
-            AccountBalance_Clear();
-            AccountBalance_Insert(bal);
+            // delete all entries from the accountbalance table
+            AccountBalanceClear();
+
+            // save this set of balances to the table
+            AccountBalanceInsert(bal);
         }
 
+        /// <summary>
+        /// gets the value is USD of an assetpair from the database
+        /// </summary>
+        /// <param name="assetpair">XXBTCZUSD for example</param>
+        /// <returns>Price of assetpair in dollars</returns>
         private static double GetAssetValueUSD(string assetpair)
         {
             SqlParameter[] p = new SqlParameter[1];
-            p[0] = new SqlParameter("@assetpair", SqlDbType.NVarChar, 50){ Value = assetpair };
+            p[0] = new SqlParameter("@assetpair", SqlDbType.NVarChar, 50) { Value = assetpair };
             double price = Convert.ToDouble(SqlHelper.ExecuteScalar(Config.DBConn, CommandType.StoredProcedure, "TICKER_GET_CurrentPrice", p));
             return price;
-        
         }
 
-        public static void AccountBalance_Clear()
+        /// <summary>
+        /// delete all records from accountbalance table
+        /// </summary>
+        public static void AccountBalanceClear()
         {
             SqlHelper.ExecuteNonQuery(Config.DBConn, System.Data.CommandType.StoredProcedure, "AccountBalance_DELETE_ALL");
         }
-        public static void AccountBalance_Insert(BalanceObject bal)
+
+        /// <summary>
+        /// add a recors to the accountbalance table
+        /// </summary>
+        /// <param name="bal"></param>
+        public static void AccountBalanceInsert(BalanceObject bal)
         {
+            #region reference
 
-
-            #region reference 
             //CREATE PROCEDURE[dbo].[AccountBalance_INSERT]
             // (
             // @usd money,
@@ -143,14 +170,15 @@ namespace Paul.Utils
             // @dash numeric(18,5),
             // @zec numeric(18,5),
             // @rep numeric(18,5)
-            #endregion
+
+            #endregion reference
 
             SqlParameter[] p = new SqlParameter[9];
             p[0] = new SqlParameter("@usd", SqlDbType.Decimal) { Value = Convert.ToDecimal(bal.USD) };
             p[1] = new SqlParameter("@btc", SqlDbType.Decimal) { Value = Convert.ToDecimal(bal.BTC) };
             p[2] = new SqlParameter("@ltc", SqlDbType.Decimal) { Value = Convert.ToDecimal(bal.LTC) };
             p[3] = new SqlParameter("@eth", SqlDbType.Decimal) { Value = Convert.ToDecimal(bal.ETH) };
-            p[4] = new SqlParameter("@xdg",  SqlDbType.Decimal) { Value = Convert.ToDecimal(bal.DGE) };
+            p[4] = new SqlParameter("@xdg", SqlDbType.Decimal) { Value = Convert.ToDecimal(bal.DGE) };
             p[5] = new SqlParameter("@xmr", SqlDbType.Decimal) { Value = Convert.ToDecimal(bal.XMR) };
             p[6] = new SqlParameter("@dash", SqlDbType.Decimal) { Value = Convert.ToDecimal(bal.DASH) };
             p[7] = new SqlParameter("@zec", SqlDbType.Decimal) { Value = Convert.ToDecimal(bal.ZEC) };
@@ -158,13 +186,12 @@ namespace Paul.Utils
             SqlHelper.ExecuteNonQuery(Config.DBConn, CommandType.StoredProcedure, "AccountBalance_Insert", p);
         }
 
-       
-
-        public static void wait(int msec)
-        {
-            System.Threading.Thread.Sleep(msec);
-        }
-
+        /// <summary>
+        /// the accountbalance object 
+        /// an unholy union of json, c# and mexican rodeo carnies
+        /// when the app receives the JSON data fro Kraken 
+        /// newtonsoft.json deserializes the data into this handy class
+        /// </summary>
         public class Result
         {
             /// <summary>
